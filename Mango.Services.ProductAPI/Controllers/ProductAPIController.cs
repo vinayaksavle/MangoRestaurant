3f422d2943c05d24a68e815de60d0d37d20d523a
vinayak.savle@gmail.com <vinayak.savle@gmail.com>
2022-03-15 15:25:13 +0530

    Completed Product API CRUD Operations.

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -90,6 +90,7 @@ namespace Mango.Services.ProductAPI.Controllers
         }
 
         [HttpDelete]
+        [Route("{id}")]
         public async Task<object> Delete(int id)
         {
             try
