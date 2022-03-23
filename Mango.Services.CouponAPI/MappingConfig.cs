a1bd142518721748dfc2493e0b743925d866ca3d
vinayak.savle@gmail.com <vinayak.savle@gmail.com>
2022-03-23 19:23:59 +0530

    Coupon API Service

---
+++ Mango.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappings = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Coupon, CouponDto>().ReverseMap();
+                //config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                //config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                //config.CreateMap<Cart, CartDto>().ReverseMap();
+            });
+
+            return mappings;
+        }
+    }
+}
